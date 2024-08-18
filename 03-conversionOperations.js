e1c4eeeef8962ae921550df89a00dd996965a2ed
Arshpreet0991 <arsh.singh0991@gmail.com>
2024-08-18 18:34:56 +0000

    conversion of data types

---
+++ 03-conversionOperations.js
@@ -0,0 +1,76 @@
+// console.log(typeof (arg)) it is used to get the data type of a variable/object etc.
+
+// we use it a lot because lets say someone working on front end is able to take value from user and send it to back end. we cannot be 100% sure if the guy sent us a value in number or string or an object. we just know that he sent a value. for eg. let score=33 or score="33". we will see 33 in output and we will not know if it is a number or string. Therefore, typeof(arg) is very a key part to dissolve the data type confusion. Data-type consfusion is a very common problem to face during JavaScript development.
+
+// to convert the datatype according to our requirement, we use the following examples
+
+
+let score = "hitesh"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = "hitesh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// *********************** Operations ***********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+ 
