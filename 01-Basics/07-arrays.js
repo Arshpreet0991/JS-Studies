28733d55055dd2e7641ef949c2692435f663b8ab
Arshpreet0991 <arsh.singh0991@gmail.com>
2024-08-20 01:30:01 +0000

    Arrays

---
+++ 01-Basics/07-arrays.js
@@ -0,0 +1,11 @@
+// Array
+const myArr=[0,1,2,3,4,5]
+// JavaScript arrays are resizable.
+// Arrays elements can be of mixed datatype
+
+// different ways to declare arrays
+const myHeros=["batman","superman","wonderwoman"]
+
+const myArr2= new Array(1,3,5,7,3,"Ash");
+
+console.log(myArr2);
