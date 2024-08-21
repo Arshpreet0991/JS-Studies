fb7427e83f75d16d2a309d140b46522613ed0a38
Arshpreet0991 <arsh.singh0991@gmail.com>
2024-08-21 00:33:39 +0000

    functions

---
+++ 02_Basics/01_funtionsAndParameters.js
@@ -0,0 +1,25 @@
+//Functions
+/*
+function addTwoNum(n1,n2){// here n1 n2 are parameters
+console.log(n1+n2);
+}
+// the following method is not right for storing a function value into a variable. Its gives output as "undefined"
+addTwoNum(3,4) // 3,4 are arguements
+const result=addTwoNum(3,5)
+console.log("result",result);
+*/
+
+// better way of doing this
+/*function addTwoNum(n1,n2){
+    let result=n1+n2
+    return result // after return statement the code exits the funstion, so after return whatever code is there wont be executed
+}
+    */
+
+// another way of writing the code
+function addTwoNum(n1,n2){
+    return n1+n2 
+}
+const result=addTwoNum(3,5)
+console.log("result",result);
+// now the output wont be undefined. So in JS we need to use return if we want to use the value from a funtion
